439c14494af4822708f5a8762e1aaac33d78598a
Emmanuel-Rono <ronoemmanuel933@gmail.com>
2024-01-17 14:16:53 +0300

    Working On comose Modifiers

--- app/src/main/java/com/emmanuel_rono/composetut/MainActivity.kt
+++ app/src/main/java/com/emmanuel_rono/composetut/MainActivity.kt
@@ -1,65 +1,83 @@
 package com.emmanuel_rono.composetut
 
 
+import android.graphics.fonts.FontStyle
+import android.os.Build
 import android.os.Bundle
 
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.selection.SelectionContainer
 
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.emmanuel_rono.composetut.ui.theme.ComposeTutTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Surface(
                 modifier=Modifier.fillMaxSize(),
                 color=MaterialTheme.colors.background){
+                greetings()
 
             }
             }
         }
         }
+@RequiresApi(Build.VERSION_CODES.Q)
 @Composable
-fun ColumnScope.uiTest(color: Color, weight:Float)
+fun greetings() {
+    MaterialTheme {
 
-{
-    Surface(
-        modifier= Modifier
-            .width(200.dp)
-            .weight(3f)
-            .height(50.dp),
-        color=MaterialTheme.colors.primary
-    ){
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
+        )
+        {
+            Box(
+                modifier = Modifier
+                    .height(50.dp)
+                    .background(Color.Green)
+            ) {
+                SelectionContainer {
+                    Text("Hello Android")
+                    FontStyle.FONT_SLANT_ITALIC
+                    Color.Black
+                }
+
+
+
+
+            }
+        }
 
     }
 }
 
-@Composable
+
+@RequiresApi(Build.VERSION_CODES.Q)
 @Preview
-fun PreviewCard() {
-    MaterialTheme{
-        Column(modifier=Modifier.fillMaxSize()){
-            Surface(
-                modifier= Modifier
-                    .width(200.dp)
-                    .weight(3f)
-                    .height(50.dp),
-                color=MaterialTheme.colors.primary
-            ){
-                uiTest(color = MaterialTheme.colors.primary, weight = 3f  )
+@Composable
+fun greetingPreview()
+{
+    greetings()
+}
 
-            }
-    }
 
-}}
 
 
 
