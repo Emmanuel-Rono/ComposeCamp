8534b30d6364f7073fa6bbfa4ddd2deee72361fd
Emmanuel-Rono <ronoemmanuel933@gmail.com>
2024-01-23 15:18:05 +0300

    Working on Textfield

--- app/src/main/java/com/emmanuel_rono/composetut/MainActivity.kt
+++ app/src/main/java/com/emmanuel_rono/composetut/MainActivity.kt
@@ -8,9 +8,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 import androidx.compose.ui.tooling.preview.Preview
 
@@ -28,8 +33,16 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting()
 {
-    TextField(value = "Type Here ...", onValueChange ={} )
-}
+    Column(
+        modifier= Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally)
+    {
+        TextField(value = "Type Here ...", onValueChange = {},
+    label={
+        Text(text="Name")
+})}}
+
 @RequiresApi(Build.VERSION_CODES.Q)
 @Preview
 @Composable
